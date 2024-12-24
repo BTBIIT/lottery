372d4437a76a83f7ee2fbf8a5c4709f602e70ff7
ysh <ysh940129@gmail.com>
2024-12-24 17:03:05 +0900

    Fix build files and prepare for deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  base: "/",
+  build: {
+    outDir: "build", // 빌드 결과물이 생성될 폴더 이름 설정
+  },
 });
