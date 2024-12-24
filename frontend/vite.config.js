5ab7ac8ec5f893e9fcda2455a381b3546dab29d0
ysh <ysh940129@gmail.com>
2024-12-24 17:31:27 +0900

    Add build files

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,10 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: "/lottery/", // GitHub Pages에서 이 프로젝트의 경로에 맞게 설정
   build: {
     outDir: "build", // 빌드 결과물이 생성될 폴더 이름 설정
   },
